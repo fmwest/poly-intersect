f363d6aa7f3d8604e2d1d6d10d6adf65cacb3dff
bcollins <parietal.io@gmail.com>
2017-07-22 12:05:37 -0500

    fixed string formatting function

--- polyIntersect/utils/files.py
+++ polyIntersect/utils/files.py
@@ -8,7 +8,7 @@ BASE_DIR = path.dirname(PROJECT_DIR)
 def load_config_json(name):
     json_path = path.abspath(path.join(BASE_DIR,
                                        'microservice',
-                                       '{name}.json'))
+                                       f'{name}.json'))
     with open(json_path) as data_file:
         info = json.load(data_file)
     return info
