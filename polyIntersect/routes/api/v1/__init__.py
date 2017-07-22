377ee96430c5424c62a233a22adff5f5f80646d4
bcollins <parietal.io@gmail.com>
2017-07-22 11:37:19 -0500

    pep8 fixes and removed __future__ imports

--- polyIntersect/routes/api/v1/__init__.py
+++ polyIntersect/routes/api/v1/__init__.py
@@ -1,13 +1,5 @@
-"""-"""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from flask import Blueprint, jsonify
 
-
-# GENERIC Error
 
 def error(status=400, detail='Bad Request'):
     return jsonify(errors=[{
@@ -15,5 +7,6 @@ def error(status=400, detail='Bad Request'):
         'detail': detail,
         'david': 'david'
     }]), status
+
 
 endpoints = Blueprint('endpoints', __name__)
