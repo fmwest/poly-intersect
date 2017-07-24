7686cfc63b30c288c04d597fbee87dae917f7f99
fiona <fwest@blueraster.com>
2017-07-24 12:19:07 -0400

    Edited test functions

--- tests/test_analysis_funcs.py
+++ tests/test_analysis_funcs.py
@@ -4,19 +4,18 @@ from polyIntersect.micro_functions.poly_intersect import json2ogr
 from polyIntersect.micro_functions.poly_intersect import dissolve
 from polyIntersect.micro_functions.poly_intersect import intersect
 
-from .sample_data import DISSOLVE_ME
+from .sample_data import DISSOLVE_GEOJSON
 
 fixtures = path.abspath(path.join(path.dirname(__file__), 'fixtures'))
 
 
 def test_dissolve_successful():
     import pdb;pdb.set_trace()
-    geom = json2ogr(DISSOLVE_ME)
-    geom_diss = dissolve(geom, 'str_value')
-    assert geom_diss.GetGeometryCount() == 2
+    geom = json2ogr(DISSOLVE_GEOJSON)
+    geom_diss = dissolve(geom)
+    assert geom_diss.GetGeometryCount() == 1
 
 
 def test_intersect_successful():
     import pdb;pdb.set_trace()
-    geom_intersect = intersect(self_intersect, self_intersect)
-    assert geom_intersect
+    assert False
