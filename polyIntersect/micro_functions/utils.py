751305312d8fe342b44938bba0bb509893ab219f
fiona <fwest@blueraster.com>
2017-07-25 18:31:56 -0400

    pep8 edits on Davids updates to the develop branch

--- polyIntersect/micro_functions/utils.py
+++ polyIntersect/micro_functions/utils.py
@@ -1,9 +1,7 @@
 from osgeo import ogr, osr
 import geojson as gj
-from geomet import wkt as WKT
 import urllib3
 import polyIntersect.micro_functions.urls as urls
-
 
 
 def verify_polygons(in_json):
@@ -23,25 +21,31 @@ def verify_polygons(in_json):
     return
 
 
-def dissolve_to_single_feature(in_json):
+def json_polys_to_ogr(in_json):
     polys = ogr.Geometry(ogr.wkbMultiPolygon)
     for feature in gj.loads(in_json)['features']:
         poly = ogr.CreateGeometryFromJson(gj.dumps(feature['geometry']))
         polys.AddGeometry(poly)
-    
+
     return polys
 
 
-def buffer_and_dissolve_to_single_feature(in_json, distance):
+def dissolve_ogr_to_single_feature(ogr_geom):
+    dissolved = ogr_geom.UnionCascaded()
+
+    return dissolved
+
+
+def buffer_ogr_polygons(ogr_geom, distance, dissolve=True, original_epsg=4326):
     buffers = ogr.Geometry(ogr.wkbMultiPolygon)
-    
-    if ogr_geom.GetGeometryType() == 6: # Geometry type 6 is MultiPolygon    
+
+    if ogr_geom.GetGeometryType() == 6:  # Geometry type 6 is MultiPolygon
         parts = ogr_geom.GetGeometryCount()
         for i in range(0, parts):
             geom = ogr_geom.GetGeometryRef(i)
             buffers.AddGeometry(build_buffer(geom, distance, original_epsg))
 
-    else: # Geometry type must be Polygon (type 3) here
+    else:  # Geometry type must be Polygon (type 3) here
         buff = build_buffer(ogr_geom, distance, original_epsg)
         buffers.AddGeometry(buff)
 
@@ -51,7 +55,7 @@ def buffer_and_dissolve_to_single_feature(in_json, distance):
     return buffers_dissolved
 
 
-def project(ogr_geom, centroid, direction, original_epsg=4326):
+def project(ogr_geom, centroid, direction, original_epsg):
     wkt_proj = 'PROJCS["World_Azimuthal_Equidistant_custom_center", \
                     GEOGCS["GCS_WGS_1984", \
                         DATUM["WGS_1984", \
@@ -68,7 +72,7 @@ def project(ogr_geom, centroid, direction, original_epsg=4326):
                     .format(centroid.GetX(), centroid.GetY())
 
     original_sr = osr.SpatialReference()
-    original_sr.ImportFromEPSG(original_epsg)  # 4326 = WGS84
+    original_sr.ImportFromEPSG(original_epsg)
 
     target_sr = osr.SpatialReference()
     target_sr.ImportFromWkt(wkt_proj)
@@ -78,22 +82,19 @@ def project(ogr_geom, centroid, direction, original_epsg=4326):
     elif direction.lower() == 'to-original':
         transform = osr.CoordinateTransformation(target_sr, original_sr)
     else:
-        msg = ("utils.project 'direction' parameter invalid."
-               "Must be either 'to-original' or 'to-custom'")
-        raise Exception(msg)
+        raise Exception('utils.project \'direction\' parameter invalid. \
+                         Must be either \'to-original\' or \'to-custom\'')
 
     ogr_geom.Transform(transform)
 
     return ogr_geom
 
 
-def build_buffer(json_in, distance, original_epsg=4326,
-                 export_as='OGR', return_to_original_sr=True):
-    wkt = WKT.dumps(gj.loads(json_in))
-    poly = ogr.CreateGeometryFromWkt(wkt)
-    centroid = poly.Centroid()
+def build_buffer(ogr_geom, distance, original_epsg, export_as='OGR',
+                 return_to_original_sr=True):
+    centroid = ogr_geom.Centroid()
 
-    poly_prj = project(poly, centroid, 'to-custom')
+    poly_prj = project(ogr_geom, centroid, 'to-custom', original_epsg)
 
     buff = poly_prj.Buffer(distance)
 
@@ -118,68 +119,73 @@ def build_buffer(json_in, distance, original_epsg=4326,
 def calculate_dissolved_area(ogr_geom, original_epsg=4326):
     area_m2 = 0
 
-    if parts > 1:
-        for i in range(parts):
-            geom = ogr_geom.GetGeometryRef(i)
-            area_part = project(geom, geom.Centroid(), 'to-custom').GetArea()
+    if ogr_geom.GetGeometryType() == 6:  # Geometry type 6 is MultiPolygon
+        parts = ogr_geom.GetGeometryCount()
+        for i in range(0, parts):
+            geom = ogr_geom.GetGeometryRef(i)  # Reads geom as LinearRing
+            area_part = project(geom, geom.Centroid(), 'to-custom',
+                                original_epsg).GetArea()
             area_m2 += area_part
-    else:
-        area_m2 = project(ogr_geom, ogr_geom.Centroid(), 'to-custom').GetArea()
 
-    else: # Geometry type must be Polygon (type 3) here
-        area_m2 = project(ogr_geom, ogr_geom.Centroid(), 'to-custom', original_epsg).GetArea()
-    
+    else:  # Geometry type must be Polygon (type 3) here
+        area_m2 = project(ogr_geom, ogr_geom.Centroid(), 'to-custom',
+                          original_epsg).GetArea()
+
     area_ha = area_m2 * 0.0001
-    
+
     return area_ha
 
 
-# get_min_max_xy assumes ogr_geom is passed in with decimal degrees as the linear unit,
-# so that decimal degrees are returned as min/max x/y
+# get_min_max_xy assumes ogr_geom is passed in with decimal degrees
+# as the linear unit, so that decimal degrees are returned as min/max x/y
 def get_min_max_xy(ogr_geom):
     envelope = ogr_geom.GetEnvelope()
     return envelope
 
-def get_intersect_geom_from_endpoint(ogr_geom, layer, fields, original_epsg=4326):
-    #####=====  GET ENVELOPE OF MIN/MAX X/Y OF USER  =====##### 
-    #####===== POLYGONS TO EXTRACT GEOM FROM SERVICE =====#####
+
+def get_intersect_geom_from_endpoint(ogr_geom, layer, fields,
+                                     original_epsg=4326):
+    # =====  GET ENVELOPE OF MIN/MAX X/Y OF USER  ===== #
+    # ===== POLYGONS TO EXTRACT GEOM FROM SERVICE ===== #
     minX, maxX, minY, maxY = get_min_max_xy(ogr_geom)
-    
-    #####===== TRY TO GET SPATIAL REFERENCE OF USER POLYGONS  =====##### 
+
+    # ===== TRY TO GET SPATIAL REFERENCE OF USER POLYGONS  ===== #
     sr = ogr_geom.GetSpatialReference()
     if not sr:
         sr = original_epsg
 
-    geom = {'xmin' : minX,\
-            'ymin' : minY, \
-            'xmax' : maxX, \
-            'ymax' : maxY, \
-            'spatialReference' : {'wkid' : sr}}
+    geom = {'xmin': minX,
+            'ymin': minY,
+            'xmax': maxX,
+            'ymax': maxY,
+            'spatialReference': {'wkid': sr}}
 
-    query = {'where'          : '1=1',
-             'geometry'       : geom,
-             'geometryType'   : 'esriGeometryEnvelope', 
-             'spatialRel'     : 'esriSpatialRelIntersects',
-             'outFields'      : '*',
-             'returnGeometry' : 'true',
-             'outSR'          : '',
-             'f'              : 'geojson'}
+    query = {'where': '1=1',
+             'geometry': geom,
+             'geometryType': 'esriGeometryEnvelope',
+             'spatialRel': 'esriSpatialRelIntersects',
+             'outFields': '*',
+             'returnGeometry': 'true',
+             'outSR': '',
+             'f': 'geojson'}
 
     http = urllib3.PoolManager()
-    
-    if layer=='gadmAdm2':
+
+    if layer == 'gadmAdm2':
         intersect_polys = http.request('GET', urls.gadmAdm2, fields=query)
 
-    elif layer=='gadmAdm1':
+    elif layer == 'gadmAdm1':
         intersect_polys = http.request('GET', urls.gadmAdm1, fields=query)
 
-    elif layer=='gadmAdm0':
+    elif layer == 'gadmAdm0':
         intersect_polys = http.request('GET', urls.gadmAdm0, fields=query)
-    
-    elif layer=='TreePlantations':
-        intersect_polys = http.request('GET', urls.TreePlantations, fields=query)
+
+    elif layer == 'TreePlantations':
+        intersect_polys = http.request('GET', urls.TreePlantations,
+                                       fields=query)
 
     else:
-        raise AssertionError('Specified intersect_layer ({}) does not exist.'.format(layer))
+        raise AssertionError('Specified intersect_layer ({}) \
+                              does not exist.'.format(layer))
 
     return gj.dumps(gj.loads(intersect_polys.data.decode('utf-8')))
