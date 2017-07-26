61dc0a1e6c7213e5ffc20dd774031a741e43ee6d
bcollins <parietal.io@gmail.com>
2017-07-26 10:10:22 -0500

    removed some spaces

--- tests/sample_data.py
+++ tests/sample_data.py
@@ -15,39 +15,31 @@ def download_sample_data():
 
     for name in big_files:
         url = path.join(repo, 'blob/master', name + '?raw=true')
-        print(url)
         gzip_path = path.join(fixtures, name)
         output_path = path.join(fixtures, name.replace('.gz', ''))
 
         urlretrieve(url, gzip_path)
         with gzip.open(gzip_path, 'rb') as f:
             with open(output_path, 'wb') as outf:
-                print(output_path)
                 outf.write(f.read())
                 print('added sample data: {}'.format(name))
 
         remove(gzip_path)
 
-
 with open(path.join(fixtures, 'maine.geojson')) as f:
     MAINE_GEOJSON = "".join(f.read().split())
-
 
 with open(path.join(fixtures, 'self-intersecting.geojson')) as f:
     SELF_INTERSECTING_GEOJSON = "".join(f.read().split())
 
-
 with open(path.join(fixtures, 'dissolve_me.geojson')) as f:
     DISSOLVE_GEOJSON = "".join(f.read().split())
-
 
 with open(path.join(fixtures, 'intersect_base.geojson')) as f:
     INTERSECT_BASE_GEOJSON = "".join(f.read().split())
 
-
 with open(path.join(fixtures, 'intersect_fully_within.geojson')) as f:
     INTERSECT_FULLY_WITHIN_GEOJSON = "".join(f.read().split())
-
 
 with open(path.join(fixtures, 'intersect_partially_within.geojson')) as f:
     INTERSECT_PARTIALLY_WITHIN_GEOJSON = "".join(f.read().split())
