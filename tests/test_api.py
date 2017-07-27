348944cddd78f22b4a56feda118e4dc5093cfd21
bcollins <parietal.io@gmail.com>
2017-07-26 20:07:40 -0500

    removed older graph test

--- tests/test_api.py
+++ tests/test_api.py
@@ -21,40 +21,17 @@ slow = pytest.mark.skipif(
 )
 
 
-def test_execute_graph():
-    url = '/api/v1/polyIntersect/executeGraph?'
-
-    feats1 = json.loads(INTERSECT_PARTIALLY_WITHIN_GEOJSON)
-    feats2 = json.loads(INTERSECT_BASE_GEOJSON)
-
-    graph = {'convert_aoi': ['geojson', json.loads(DISSOLVE_GEOJSON)],
-             'convert_intersect_partial': ['geojson', feats1],
-             'convert_intersect_base': ['geojson', feats2],
-             'dissolve_aoi': ['dissolve', 'convert_aoi', 'str_value'],
-             'intersect_aoi': ['intersect',
-                               'convert_intersect_partial',
-                               'convert_intersect_base']}
-
-    payload = {}
-    payload['dag'] = json.dumps(graph)
-    payload['result_keys'] = json.dumps(['intersect_aoi', 'dissolve_aoi'])
-
-    result = app.post(url, data=payload)
-    result_obj = json.loads(result.data)
-
+def test_hello():
+    url = '/api/v1/polyIntersect/hello?'
+    result = app.get(url)
     assert result.status_code == 200
-    assert result.content_type == 'application/json'
-    assert 'intersect_aoi' in list(result_obj.keys())
-    assert 'dissolve_aoi' in list(result_obj.keys())
 
 
 def test_example_graph():
     url = '/api/v1/polyIntersect/brazil-biomes?'
 
-    feats1 = json.loads(BRAZIL_USER_POLY)
-
     payload = {}
-    payload['user_json'] = json.dumps(feats1)
+    payload['user_json'] = BRAZIL_USER_POLY
     payload['category'] = 'name'
 
     result = app.post(url, data=payload)
